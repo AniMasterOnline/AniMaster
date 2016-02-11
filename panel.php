33e1abf059aa3c5031210cb789f68ea4d697edf8
smusaran <marcgomez@iesmontsia.org>
2016-02-11 15:24:01 +0100

    Inex commit

---
+++ panel.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Panel</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <LINK REL=StyleSheet HREF="public/css/bodyStyle.css" TYPE="text/css" MEDIA=screen>
+        <LINK REL=StyleSheet HREF="public/css/headerStyle.css" TYPE="text/css" MEDIA=screen>
+        <LINK REL=StyleSheet HREF="public/css/contentStyle.css" TYPE="text/css" MEDIA=screen>
+        <LINK REL=StyleSheet HREF="public/css/footerStyle.css" TYPE="text/css" MEDIA=screen>
+    </head>
+    <body>
+        <!-- Debug Mode -->
+        <?php include "system/Errors.php"; ?>
+        
+        <!-- Header content box -->
+        <?php include "public/layouts/menu.php";?>
+        
+        <!-- Body box -->
+        <div class="body-box">
+            <div class="content-box-panel">
+                <p>Hola</p>
+            </div>
+        </div>
+    </body>
+</html>
