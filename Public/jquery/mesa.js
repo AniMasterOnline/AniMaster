eaff94cfed4f6e4ab05919a8605e7415d352cc53
NeoDaark <DaarkAssassin@gmail.com>
2016-02-26 21:37:17 +0100

    New Funcionality - open close Multimedia nav

--- Public/jquery/mesa.js
+++ Public/jquery/mesa.js
@@ -9,4 +9,34 @@ $(document).ready(function(){
     $("#eyeobj").click(function(){
         $("#taulobj").toggle(500);
     });
+    $("#sidebar-toggle").click(function(){
+        var mpanelbox = $('#mpanel-box').attr('class');
+        var mesamultimedia = $('#mesa-multimedia').attr('class');
+        var mesacontent = $('#mesa-content').attr('class');
+        
+        if (mpanelbox == "mpanel-box-open"){
+            $("#mpanel-box").removeClass("mpanel-box-open");
+            $("#mpanel-box").addClass("mpanel-box-closed");
+        }else{
+            $("#mpanel-box").removeClass("mpanel-box-closed");
+            $("#mpanel-box").addClass("mpanel-box-open");
+        }
+        if (mesamultimedia == "mesa-multimedia-open"){
+           $("#mesa-multimedia").removeClass("mesa-multimedia-open");
+            $("#mesa-multimedia").addClass("mesa-multimedia-closed");
+        }else{
+            $("#mesa-multimedia").removeClass("mesa-multimedia-closed");
+            $("#mesa-multimedia").addClass("mesa-multimedia-open");
+        }
+        if (mesacontent == "mesa-content-open"){
+           $("#mesa-content").removeClass("mesa-content-open");
+            $("#mesa-content").addClass("mesa-content-closed");
+        }else{
+            $("#mesa-content").removeClass("mesa-content-closed");
+            $("#mesa-content").addClass("mesa-content-open");
+        }
+        
+        
+    });
+    
 });
--- mesav2.php
+++ mesav2.php
@@ -11,13 +11,20 @@
         <!-- Header content box -->
         <?php include "Public/layouts/menu.php";?>
         <!-- Panel content box -->
-        <div class="mpanel-box">
+        <div id="mpanel-box" class="mpanel-box-open">
             <div class="mpanel-nav">
                 <ul class="mnav">
-                    <li><a> <img src="Public/img/mesa/eye.png" class="eye"/> Monstres</a></li>
-                    <li><a> <img src="Public/img/mesa/eye.png" class="eye"/> Jugadors</a></li>
-                    <li><a> <img src="Public/img/mesa/eye.png" class="eye"/> Objectes</a></li>
-                    
+                    <li><a id="eyemonst"> <img src="Public/img/mesa/eye.png" class="eye"/> Monstres</a></li>
+                    <li><a id="eyepj"> <img src="Public/img/mesa/eye.png" class="eye"/> Jugadors</a></li>
+                    <li><a id="eyeobj"> <img src="Public/img/mesa/eye.png" class="eye"/> Objectes</a></li>
+                    <li style="float:right;">
+                        <a id="sidebar-toggle">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                        </a>
+                    </li>
                     <li style="float:right;"><a> <img src="Public/img/mesa/vida.png" class="accio"/> </a></li>
                     <li style="float:right;"><a> <img src="Public/img/mesa/daus.png" class="accio"/> </a></li>
                     <li style="float:right;"><a> <img src="Public/img/mesa/batalla2.png" class="accio"/> </a></li>
@@ -26,7 +33,7 @@
             </div>
         </div>
         <!-- Aside Multimedia content -->
-        <div class="mesa-multimedia">
+        <div id="mesa-multimedia" class="mesa-multimedia-open">
             <div class="hang"><p>HANGOUT</p></div>
             <div class="chat"><p>CHAT</p></div>
             <div class="musica">
@@ -42,55 +49,53 @@
             </div>
         </div>
         <!-- Body content box -->
-        <div class="mesa-content">
-            <div id="taulmonst" class="fitxam">
-            <div>
-                <table class="llista">
-                    <tr>
-                        <td class="selfit">1º Enemic</td>
-                        <td class="selfit">2º Enemic</td>
-                        <td class="selfit">3º Enemic</td>
-                        <td class="selfit">4º Enemic</td>
-                    </tr>
-                </table>
-            </div>
-            <div class="todofitxa">
-                <div class="dalt">
-                    <div class="imatgefit"></div>
-                        <div class="dadaph"></div>
-                        <div class="dadatrn"></div>
-                        <div class="dadaatk"></div>
-                        <div class="dadadany"></div>
-                        <div class="dadadef"></div>
-                        <div class="dadata"></div>
-                </div>
-                <div class="centre">
-                    <table>
+        <div id="mesa-content" class="mesa-content-open">
+            <div id="taulmonst" class="fitxa-closed">
+                <div>
+                    <table class="llista">
                         <tr>
-                            <td>poderes</td>
+                            <td class="selfit">1º Enemic</td>
+                            <td class="selfit">2º Enemic</td>
+                            <td class="selfit">3º Enemic</td>
+                            <td class="selfit">4º Enemic</td>
                         </tr>
                     </table>
                 </div>
-                <div class="baix">
-                    <div class="modus"></div>
+                <div class="todofitxa">
+                    <div class="dalt">
+                        <div class="imatgefit"></div>
+                            <div class="dadaph"></div>
+                            <div class="dadatrn"></div>
+                            <div class="dadaatk"></div>
+                            <div class="dadadany"></div>
+                            <div class="dadadef"></div>
+                            <div class="dadata"></div>
+                    </div>
+                    <div class="centre">
+                        <table>
+                            <tr>
+                                <td>poderes</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="baix">
+                        <div class="modus"></div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="taulpj" class="fitxap">
-            Players
-        </div>
-        <div id="taulobj" class="fitxao">
-            Objectes
-        </div>
+            <div id="taulpj" class="fitxa-closed">
+                player
+            </div>
+            <div id="taulobj" class="fitxa-closed">
+                objectes
+            </div>
+            <div id="footer">
+                <a href="copyright.html">@Copyright by AniMasterOnline</a>
+            
+            </div>
         </div>
         
         <!-- Footer content box --> 
-        <div id="footer">
-            <p>
-                <a href="copyright.html">@Copyright by AniMasterOnline</a>
-            </p>
-        </div>
-
         
     </body>
 </html>
