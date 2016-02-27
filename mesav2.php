4ebfdd414062a19de7307053d80a3d446a7ed493
NeoDaark <DaarkAssassin@gmail.com>
2016-02-27 19:32:13 +0100

    Css edited in Mi Mesa

--- mesav2.php
+++ mesav2.php
@@ -60,14 +60,12 @@
             <div id="taulmonst" class="fitxa-open">
                 <h2 class="fitxa-title"><center>Enemics</center></h2>
                 <div class="llista">
-                    <table>
-                        <select name="npc" class="selfit">
-                            <option value="1">Soldado</option>
-                            <option value="2">Lobo</option>
-                            <option value="3">Araña gigante</option>
-                            <option value="4">Chutlu</option>
-                        </select>
-                    </table>
+                    <select name="npc" class="selfit">
+                        <option value="1">Soldado</option>
+                        <option value="2">Lobo</option>
+                        <option value="3">Araña gigante</option>
+                        <option value="4">Chutlu</option>
+                    </select>
                 </div>
                 <div class="todofitxa">
                     <div class="dalt">
@@ -109,6 +107,7 @@
                                 <div class="column3">000</div>
                             </div>
                         </div>
+                        <button class="modfitxa">Modifica!</button>
                     </div>
                     <div class="centre">
                         <br><br><br><br><br>
@@ -126,14 +125,12 @@
             <div id="taulpj" class="fitxa-open">
                 <h2 class="fitxa-title"><center>Jugadors</center></h2>
                 <div class="llista">
-                    <table>
-                        <select name="pj" class="selfit">
-                            <option value="1">Marc</option>
-                            <option value="2">Eduard</option>
-                            <option value="3">Pau</option>
-                            <option value="4">David</option>
-                        </select>
-                    </table>
+                    <select name="pj" class="selfit">
+                        <option value="1">Marc</option>
+                        <option value="2">Eduard</option>
+                        <option value="3">Pau</option>
+                        <option value="4">David</option>
+                    </select>
                 </div>
                 <div class="todofitxa">
                     <div class="dalt">
@@ -175,6 +172,7 @@
                                 <div class="column3">000</div>
                             </div>
                         </div>
+                        <button class="modfitxa">Modifica!</button>
                     </div>
                     <div class="centre">
                         <br><br><br><br><br>
@@ -192,14 +190,12 @@
             <div id="taulobj" class="fitxa-open">
                 <h2 class="fitxa-title"><center>Objectes</center></h2>
                 <div class="llista">
-                    <table>
-                        <select name="obj" class="selfit">
-                            <option value="1">obj - 1</option>
-                            <option value="2">obj - 2</option>
-                            <option value="3">obj - 3</option>
-                            <option value="4">obj - 4</option>
-                        </select>
-                    </table>
+                    <select name="obj" class="selfit">
+                        <option value="1">obj - 1</option>
+                        <option value="2">obj - 2</option>
+                        <option value="3">obj - 3</option>
+                        <option value="4">obj - 4</option>
+                    </select>
                 </div>
                 <div class="todofitxa">
                     <div class="dalt">
@@ -209,9 +205,12 @@
                                 <div class="column1">Descripció</div>
                             </div>
                             <div class="row-bottom">
+                                <div class="column3">
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </div>
                             </div>
                         </div>
+                        <button class="modfitxa">Modifica!</button>
                     </div>
                     <div class="centre">
                         <br><br><br><br><br>
@@ -228,8 +227,9 @@
             </div>
             <!-- Footer content box --> 
             <div id="footer">
-                <a href="copyright.html">@Copyright by AniMasterOnline</a>
-            
+                <center>
+                <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">AniMasterOnline</span> de <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Marc, Jacob i David</span> està subjecta a una llicència de <a class="link" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Reconeixement-NoComercial-CompartirIgual 4.0 Internacional de Creative Commons</a>
+                </center>
             </div>
     </body>
 </html>
