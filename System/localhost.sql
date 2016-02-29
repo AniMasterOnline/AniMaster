6521c37badad16e0e946b615094d02dde8962b3c
smusaran <marcgomez@iesmontsia.org>
2016-02-29 16:50:09 +0100

    BBDD marc 16:50

--- System/localhost.sql
+++ System/localhost.sql
@@ -1,13 +1,26 @@
+--
 -- Base de datos: `DB_AniMaster`
+--
 CREATE DATABASE IF NOT EXISTS `DB_AniMaster` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `DB_AniMaster`;
 
--- --------------------------------------------------------
+--
+-- TABLE: `Partida`
+--
+DROP TABLE IF EXISTS `Partida`;
+CREATE TABLE IF NOT EXISTS `Partida` (
+  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `titol` varchar(32) NOT NULL,
+  `descripcio` varchar(250) NOT NULL,
+  `any_partida` int(10) NOT NULL,
+  `nivel_sobrenatural` varchar(32) DEFAULT NULL COMMENT 'Animaster v2.0',
+  `joc` varchar(32) DEFAULT NULL COMMENT 'Animaster v2.0',
+  PRIMARY KEY (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Estructura de tabla para la tabla `Usuari`
+-- TABLE: `Usuari`
 --
-
 DROP TABLE IF EXISTS `Usuari`;
 CREATE TABLE IF NOT EXISTS `Usuari` (
   `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
@@ -17,38 +30,60 @@ CREATE TABLE IF NOT EXISTS `Usuari` (
   `telefon` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id_usuari`),
   UNIQUE KEY `user` (`user`,`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-DROP TABLE IF EXISTS `Partida`;
+INSERT INTO `Usuari` (`id_usuari`, `user`, `password`, `email`, `telefon`) VALUES
+(1, 'NeoDaark', '256c7f6aa9aeb56d4d08865c163635d0', 'DaarkAssassin@gmail.com', '681254694');
 
-CREATE TABLE IF NOT EXISTS `Partida` (
-  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
-  `titol` varchar(30) NOT NULL,
-  `descripcio` varchar(150) NOT NULL,
-  `any_partida` real(5) NOT NULL,
-  `nivell_sobrenatural` real(1) DEFAULT NULL,
-  `joc` varchar(40) NOT NULL,
-  PRIMARY KEY (`id_partida`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+--
+-- TABLE: `Usuari_Partida`
+--
 DROP TABLE IF EXISTS `Usuari_Partida`;
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 CREATE TABLE IF NOT EXISTS `Usuari_Partida` (
-  `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
-  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `id_usuari` int(10) NOT NULL PRIMARY KEY,
+  `id_partida` int(10) NOT NULL UNIQUE,
   `master` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_usuari`),
-  FOREIGN KEY (`id_partida`)
+  FOREIGN KEY (`id_usuari`) REFERENCES `Usuari` (`id_usuari`),
+  FOREIGN KEY (`id_partida`) REFERENCES `Partida` (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+--
+-- TABLE: `Enemigo`
+--
 DROP TABLE IF EXISTS `Enemigo`;
-
 CREATE TABLE IF NOT EXISTS `Enemigo` (
-  `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
-  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
-  `master` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_usuari`),
-  FOREIGN KEY (`id_partida`)
+  `id_enemigo` int(10) NOT NULL PRIMARY KEY,
+  `id_equipo` int(10) NOT NULL UNIQUE,
+  FOREIGN KEY (`id_equipo`) REFERENCES `Equipo` (`id_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+--
+-- TABLE: `Partida_Enemigo`
+--
+DROP TABLE IF EXISTS `Partida_Enemigo`;
+CREATE TABLE IF NOT EXISTS `Partida_Enemigo` (
+  `id_enemigo` int(10) NOT NULL UNIQUE,
+  `id_partida` int(10) NOT NULL PRIMARY KEY,
+  FOREIGN KEY (`id_enemigo`) REFERENCES `Enemigo` (`id_enemigo`),
+  FOREIGN KEY (`id_partida`) REFERENCES `Partida` (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- TABLE: `Equipo`
+--
+DROP TABLE IF EXISTS `Equipo`;
+CREATE TABLE IF NOT EXISTS `Equipo` (
+  `id_equipo` int(10) NOT NULL PRIMARY KEY,
+  `armadura1` number(2) NULL,
+  `armadura2` number(2) NULL,
+  `arma1` number(3) NULL,
+  `arma2` number(3) NULL,
+  FOREIGN KEY (`id_equipo`) REFERENCES `Equipo` (`id_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `Equipo` (`id_equipo`, `armadura1`, `armadura2`, `arma1`, `arma2`) VALUES
+(1, 2, NULL, 50, NULL);
+INSERT INTO `Equipo` (`id_equipo`, `armadura1`, `armadura2`, `arma1`, `arma2`) VALUES
+(2, 1, NULL, 40, NULL);
+INSERT INTO `Equipo` (`id_equipo`, `armadura1`, `armadura2`, `arma1`, `arma2`) VALUES
+(3, 4, NULL, 45, NULL);
