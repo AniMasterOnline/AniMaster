cd6047bace7784b3d6b992ed3094b6dff3cb5c29
NeoDaark <DaarkAssassin@gmail.com>
2016-02-29 15:11:26 +0100

    BD - AniMaster

--- System/localhost.sql
+++ System/localhost.sql
@@ -1,24 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 10-02-2016 a las 19:46:12
--- Versión del servidor: 5.5.46-0ubuntu0.14.04.2
--- Versión de PHP: 5.5.9-1ubuntu4.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
 -- Base de datos: `DB_AniMaster`
---
 CREATE DATABASE IF NOT EXISTS `DB_AniMaster` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `DB_AniMaster`;
 
@@ -39,6 +19,4 @@ CREATE TABLE IF NOT EXISTS `Usuari` (
   UNIQUE KEY `user` (`user`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
