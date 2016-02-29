11c7f23aff1d18079ad783b38e2c445f6cd6b733
smusaran <marcgomez@iesmontsia.org>
2016-02-29 15:47:09 +0100

    BBDD marc 15:47

--- System/localhost.sql
+++ System/localhost.sql
@@ -19,4 +19,36 @@ CREATE TABLE IF NOT EXISTS `Usuari` (
   UNIQUE KEY `user` (`user`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `Partida`;
 
+CREATE TABLE IF NOT EXISTS `Partida` (
+  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `titol` varchar(30) NOT NULL,
+  `descripcio` varchar(150) NOT NULL,
+  `any_partida` real(5) NOT NULL,
+  `nivell_sobrenatural` real(1) DEFAULT NULL,
+  `joc` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_partida`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `Usuari_Partida`;
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+CREATE TABLE IF NOT EXISTS `Usuari_Partida` (
+  `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
+  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `master` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_usuari`),
+  FOREIGN KEY (`id_partida`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+DROP TABLE IF EXISTS `Enemigo`;
+
+CREATE TABLE IF NOT EXISTS `Enemigo` (
+  `id_usuari` int(10) NOT NULL AUTO_INCREMENT,
+  `id_partida` int(10) NOT NULL AUTO_INCREMENT,
+  `master` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_usuari`),
+  FOREIGN KEY (`id_partida`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
