b6b7898c1206be930a402ed67f3e583e3169c895
NeoDaark <DaarkAssassin@gmail.com>
2016-03-04 19:46:05 +0100

    Mesa selector updated css

--- Public/jquery/load-mesa.js
+++ Public/jquery/load-mesa.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
     //console.log(jsonPartida);
     //console.log(Object.keys(jsonPartida).length);
-    
-     $("#selpart").change(function() {
+    console.log(jsonPartida);
+    $( ".input-partida-bottom" ).prop( "disabled", true );
+    $( ".input-partida" ).prop( "disabled", true ); 
+    $("#selpart").change(function() {
         var $this = $(this),
         $value = $this.val();
         var selected = $("#selpart option:selected" ).text();
-        console.log(selected);
+        console.log($value, selected);
+        $("#id_partida").val(jsonPartida[$value].id_partida);
+        $("#titol").val(jsonPartida[$value].titol);
         $("#descripcio").val(jsonPartida[$value].descripcio);
         $("#any_partida").val(jsonPartida[$value].any_partida);
         $("#nivel_sobrenatural").val(jsonPartida[$value].nivel_sobrenatural);
         $("#disabled-joc").val(jsonPartida[$value].joc);
+    });
+    $("#selPartida").click(function() {
+        var selectedEffect = "fade";
+        $("#contenidor-partida").hide(selectedEffect,500);
     });
 });
 
