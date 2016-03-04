bd9de6965ff8577c98a05ff0f75c3886bd92099a
NeoDaark <DaarkAssassin@gmail.com>
2016-03-04 20:58:13 +0100

    Panel Forms v1

--- panel.php
+++ panel.php
@@ -82,18 +82,7 @@ and open the template in the editor.
                             </select>
                             <input class="input-crea" id="logbutton" type="submit" value="Crea">
                         </form>
-                        <form method="post" action="System/Protocols/registrarEnemic.php">
-                            <h2>Crea un Enemic</h2>
-                            <input class="input-partida" id="nom" placeholder="Nom *" value="" type="text" name="TempNom" maxlength="32" required autofocus>
-                            <input class="input-partida" id="vida" placeholder="Vida *" value="" type="text" name="TempDescripcio" maxlength="250" required>
-                            <input class="input-partida" id="ataque" placeholder="Any de la partida *" value="" type="number" name="TempAny_partida" required>
-                            <input class="input-partida" id="arma" placeholder="Any de la partida *" value="" type="number" name="TempAny_partida" required>
-                            <input class="input-partida" id="defensa" placeholder="Nivell sobrenatural *" value="" type="number" name="TempNivel_sobrenatural" required>
-                            <input class="input-partida" id="armadura" placeholder="Any de la partida *" value="" type="number" name="TempAny_partida" required>
-                            <input class="input-partida" id="turno" placeholder="id_usuari *" value="'.$value['id_usuari'].'" type="hidden" name="TempId_usuari" required>
-                            <input class="input-partida" id="poderes" placeholder="id_usuari *" value="'.$value['id_usuari'].'" type="hidden" name="TempId_usuari" required>
-                            <input class="input-crea" id="logbutton" type="submit" value="Crea">
-                        </form>
+                        
                     </div>
                     <?php
                         $Usuari_Partida = new Usuari_Partida();
@@ -106,13 +95,43 @@ and open the template in the editor.
                                         <button value="'.$row->id_partida.'" onclick="desEnemic(this);">Enemic</button><button value="'.$row->id_partida.'" onclick="desNpc(this);">Npc</button><button value="'.$row->id_partida.'" onclick="desItem(this);">Items</button>
                                     </div>
                                     <div id="Enemic-'.$row->id_partida.'" class="content-panel-partida-content">
-                                        
+                                        <form method="post" action="System/Protocols/registrarEnemic.php">
+                                            <h2>Crea un Enemic</h2>
+                                            <input class="input-partida" type="hidden" name="TempId_Partida" value="'.$row->id_partida.'">
+                                            <input class="input-partida" placeholder="Nom *" type="text" name="TempNom" maxlength="32" required autofocus>
+                                            <input class="input-partida" placeholder="Vida *" type="number" name="TempVida" required>
+                                            <input class="input-partida" placeholder="Habilidad de ataque *" type="number" name="TempAtaque" required>
+                                            <input class="input-partida" placeholder="Daño de arma *" type="number" name="TempArma" required>
+                                            <input class="input-partida" placeholder="Habilidad de defensa *" type="number" name="TempDefensa" required>
+                                            <input class="input-partida" placeholder="Defensa del armadura *" type="number" name="TempArmadura" required>
+                                            <input class="input-partida" placeholder="Turno *" type="number" name="TempTurno" required>
+                                            <input class="input-partida" placeholder="Descripció dels poders" type="text" maxlength="200" name="TempPoderes">
+                                            <input class="input-crea" id="logbutton" type="submit" value="Crea">
+                                        </form>
                                     </div>
                                     <div id="Npc-'.$row->id_partida.'" class="content-panel-partida-content">
-                                        
+                                        <form method="post" action="System/Protocols/registrarPlayer.php">
+                                            <h2>Crea un NPC</h2>
+                                            <input class="input-partida" type="hidden" name="TempId_Partida" value="'.$row->id_partida.'">
+                                            <input class="input-partida" placeholder="Nom *" type="text" name="TempNom" maxlength="32" required autofocus>
+                                            <input class="input-partida" placeholder="Vida *" type="number" name="TempVida" required>
+                                            <input class="input-partida" placeholder="Habilidad de ataque *" type="number" name="TempAtaque" required>
+                                            <input class="input-partida" placeholder="Daño de arma *" type="number" name="TempArma" required>
+                                            <input class="input-partida" placeholder="Habilidad de defensa *" type="number" name="TempDefensa" required>
+                                            <input class="input-partida" placeholder="Defensa del armadura *" type="number" name="TempArmadura" required>
+                                            <input class="input-partida" placeholder="Turno *" type="number" name="TempTurno" required>
+                                            <input class="input-partida" placeholder="Descripció dels poders" type="text" maxlength="200" name="TempPoderes">
+                                            <input class="input-crea" id="logbutton" type="submit" value="Crea">
+                                        </form>
                                     </div>
                                     <div id="Item-'.$row->id_partida.'" class="content-panel-partida-content">
-                                        Item
+                                        <form method="post" action="System/Protocols/registrarPlayer.php">
+                                            <h2>Crea un Item</h2>
+                                            <input class="input-partida" type="hidden" name="TempId_Partida" value="'.$row->id_partida.'">
+                                            <input class="input-partida" placeholder="Nom *" type="text" name="TempNom"  required autofocus>
+                                            <input class="input-partida" placeholder="Descripcio *" type="text" name="TempDescripcio" maxlength="500" required>
+                                            <input class="input-crea" id="logbutton" type="submit" value="Crea">
+                                        </form>
                                     </div>
                                     ';
                             echo'</div>';
