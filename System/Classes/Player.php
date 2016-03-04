e1b93dd09aba7acb371f1aa71250b318fec492b6
smusaran <marcgomez@iesmontsia.org>
2016-03-04 20:56:26 +0100

    Classe Player.php

--- System/Classes/Player.php
+++ System/Classes/Player.php
@@ -3,10 +3,12 @@
     class Player{
         /*Atributs*/
         private $id_player;
-        private $id_equipo;
+        public $nom;
         public $vida;
         public $ataque;
+        public $arma;
         public $defensa;
+        public $armadura;
         public $turno;
         public $poderes;
         
@@ -14,12 +16,12 @@
         public function add(){
             $db = new connexio();
             $db->query("INSERT INTO Player(vida,ataque,defensa,turno,poderes,id_equipo) "
-                    . "VALUES ('$this->vida', '$this->ataque', '$this->defensa', '$this->turno', '$this->poderes', '$this->id_equipo')");
+                    . "VALUES ('$this->nom', '$this->nom','$this->vida', '$this->ataque', '$this->arma','$this->defensa', '$this->armadura','$this->turno', '$this->poderes')");
             $db->close();
         }
         public function mod(){
             $db = new connexio();
-            $db->query("UPDATE Player SET vida='$this->vida',ataque='$this->ataque',defensa='$this->defensa',turno='$this->turno',poderes='$this->poderes',id_equipo='$this->id_equipo' WHERE id_player= '$this->id_player'");
+            $db->query("UPDATE Player SET nom='$this->nom'vida='$this->vida',ataque='$this->ataque',arma='$this->arma',defensa='$this->defensa',armadura='$this->armadura',turno='$this->turno',poderes='$this->poderes' WHERE id_enemigo= '$this->id_enemigo'");
             $db->close();
         }
         public function delete($var){
@@ -33,7 +35,7 @@
             $query = $db->query($sql);
             $rtn = array();
             while($obj = $query->fetch_assoc()){
-                $Player = new Player($obj["id_player"],$obj["id_equipo"],$obj["vida"],$obj["ataque"], $obj["defensa"], $obj["turno"], $obj["poderes"]);
+                $Player = new Player($obj["id_player"],$obj["nom"],$obj["vida"],$obj["ataque"],$obj["arma"],$obj["defensa"],$obj["armadura"], $obj["turno"], $obj["poderes"]);
                 //var_dump($Usuari);
                 array_push($rtn, $Player);
             }
@@ -51,47 +53,55 @@
         }
         function __construct0(){
             $this->id_player=0;
-            $this->id_equipo="";
+            $this->nom="";
             $this->vida="";
             $this->ataque="";
+            $this->arma="";
             $this->defensa="";
+            $this->armadura="";
             $this->turno="";
             $this->poderes="";
         }
         function __construct1($a1){
             $this->id_player=$a1;
-            $this->id_equipo="";
+            $this->nom="";
             $this->vida="";
             $this->ataque="";
+            $this->arma="";
             $this->defensa="";
+            $this->armadura="";;
             $this->turno="";
             $this->poderes="";
         }
-        function __construct6($a2, $a3, $a4, $a5, $a6, $a7){
+        function __construct8($a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9){
             $this->id_player=0;
-            $this->id_equipo=$a2;
+            $this->nom=$a2;
             $this->vida=$a3;
             $this->ataque=$a4;
-            $this->defensa=$a5;
-            $this->turno=$a6;
-            $this->poderes=$a7;
+            $this->arma=$a5;
+            $this->defensa=$a6;
+            $this->armadura=$a7;
+            $this->turno=$a8;
+            $this->poderes=$a9;
         }
-        function __construct7($a1, $a2, $a3, $a4, $a5, $a6, $a7){
-            $this->id_player=$a1;
-            $this->id_equipo=$a2;
+        function __construct9($a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9){
+            $this->id_enemigo=$a1;
+            $this->nom=$a2;
             $this->vida=$a3;
             $this->ataque=$a4;
-            $this->defensa=$a5;
-            $this->turno=$a6;
-            $this->poderes=$a7;
+            $this->arma=$a5;
+            $this->defensa=$a6;
+            $this->armadura=$a7;
+            $this->turno=$a8;
+            $this->poderes=$a9;
         }
            
         //METODES SET
         public function setId_Player($var) {
             $this->id_player = $var;
         }
-        public function setId_Equipo($var) {
-            $this->id_equipo = $var;
+        public function setNom($var) {
+            $this->nom = $var;
         }
         public function setVida($var) {
             $this->vida = $var;
@@ -99,8 +109,14 @@
         public function setAtaque($var) {
             $this->ataque = $var;
         }
+        public function setArma($var) {
+            $this->arma = $var;
+        }
         public function setDefensa($var) {
             $this->defensa = $var;
+        }
+        public function setArmadura($var) {
+            $this->armadura = $var;
         }
         public function setTurno($var) {
             $this->turno = $var;
@@ -113,8 +129,8 @@
         public function getId_Player(){
             return $this->id_player;
         }
-        public function getId_Equipo(){
-            return $this->id_equipo;
+        public function getNom(){
+            return $this->nom;
         }
         public function getVida(){
             return $this->vida;
@@ -122,8 +138,14 @@
         public function getAtaque(){
             return $this->ataque;
         }
+        public function getArma(){
+            return $this->arma;
+        }
         public function getDefensa(){
             return $this->defensa;
+        }
+        public function getArmadura(){
+            return $this->armadura;
         }
         public function getTurno(){
             return $this->turno;
